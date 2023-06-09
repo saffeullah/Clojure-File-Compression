c7f854e746e955665fb8a8eb3a9414232edc3fc0
Safi Ullah <41166092+saffeullah@users.noreply.github.com>
2023-06-09 19:49:59 -0400

    Added Menu

--- src/hello.clj
+++ src/hello.clj
@@ -1,5 +1,32 @@
 (ns hello
   (:gen-class))
 
+(defn displayFile [fileName]
+
+
+
+  )
+
+(defn mainMenu []
+  (println "Select an option:")
+  (println "1. Display list of files")
+  (println "2. Display file contents")
+  (println "3. Compress a file")
+  (println "4. Uncompress a file")
+  (println "5. Exit")
+  (println "Enter your choice:  ")
+  (read)
+  )
 (defn -main []
-  (println "Hello, Worldss!"))
+  ;(println "Hello, Worldss!")
+    (let [input (mainMenu)]
+      (println "Selected option was:" input )
+      ))
+
+
+
+
+
+
+
+
