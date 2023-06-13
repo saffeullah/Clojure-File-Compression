a5dfc67726b5c72a70ed342c1c3ed91753020568
Safi Ullah <41166092+saffeullah@users.noreply.github.com>
2023-06-13 16:52:51 -0400

    Added String Compression

--- src/hello.clj
+++ src/hello.clj
@@ -1,32 +1,103 @@
 (ns hello
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
 
-(defn displayFile [fileName]
+(defn list-files [dir]
+  (let [files (io/file dir)]
+    (filter #(.isFile %) (file-seq files))))
 
+(defn current-directory-files []
+  (let [files (list-files "./src")]
+    (doseq [file files]
+      (println (.getName file)))))
 
-
+(defn show-file-contents [filename]
+  (let [updated-filename (str "./src/" filename)]
+    (with-open [reader (io/reader updated-filename)]
+      (doseq [line (line-seq reader)]
+        (println line)))
+    )
   )
 
-(defn mainMenu []
-  (println "Select an option:")
-  (println "1. Display list of files")
-  (println "2. Display file contents")
-  (println "3. Compress a file")
-  (println "4. Uncompress a file")
-  (println "5. Exit")
-  (println "Enter your choice:  ")
-  (read)
+(defn get-file-contents [filename]
+  (let [updated-filename (str "./src/" filename)]
+    (with-open [reader (io/reader updated-filename)]
+      (let [content (slurp reader)]
+        content))))
+
+
+(defn find-first-occurrence-frequency [word filename]
+  (with-open [reader (io/reader filename)]
+    (let [content (slurp reader)
+          words (clojure.string/split content #"\s+")
+          index (.indexOf (map clojure.string/lower-case words) (clojure.string/lower-case word))
+          unique-words (take-while #(not= (clojure.string/lower-case word) (clojure.string/lower-case %)) words)]
+      (if (and index (>= index 0))
+        (count (distinct unique-words))
+        word)
+
+      )
+    )
   )
+
+
+
+
+
+(defn compress-string [input-string]
+  (let [words (clojure.string/split input-string #"\s+")]
+    ;(apply str (map #(str (find-first-occurrence-frequency % "./src/frequency.txt")) words+ " "))))
+    (clojure.string/join " " (map #(str (find-first-occurrence-frequency % "./src/frequency.txt")) words))))
+
+
+
+
+
 (defn -main []
-  ;(println "Hello, Worldss!")
-    (let [input (mainMenu)]
-      (println "Selected option was:" input )
-      ))
+  ;(print "hello")
+  ;(current-directory-files)
+  ;(let [filename "abc.txt"]
+  ;  (show-file-contents filename))
+
+  ;(let [word (read-line)
+  ;      filename "./src/frequency.txt"
+  ;      frequency (find-first-occurrence-frequency word filename)]
+  ;  (println "Frequency of first occurrence:" frequency))
+
+
+    ;(let [file-string (get-file-contents "frequency.txt") ]
+    ;    (println file-string)
+    ;
+    ;  )
+    (println (compress-string "The pink elephant is absolutely groovy") )
+
+
+  ;(print (find-first-occurrence-frequency "lkjdsfjsdjk" "./src/frequency.txt"))
+  )
 
 
 
 
+;(let [input (mainMenu)]
+;  (println "Selected option was:" input ))
 
 
+;(defn mainMenu []
+;  (println "Select an option:")
+;  (println "1. Display list of files")
+;  (println "2. Display file contents")
+;  (println "3. Compress a file")
+;  (println "4. Uncompress a file")
+;  (println "5. Exit")
+;  (println "Enter your choice:  ")
+;  (let [choice (read)]
+;    (case choice
+;      1 (display-file-list)
+;      2 (display-file-contents)
+;      3 (compress-file)
+;      4 (uncompress-file)
+;      5 (println "Exiting...")
+;      (do (println "Invalid choice. Please try again.")
+;          (mainMenu)))))
 
 
